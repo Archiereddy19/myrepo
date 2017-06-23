a892ccce0f7507f46bffb260c5e53dd94abf5973
archiereddy19 <archiereddy19@gmail.com>
2017-06-22 21:44:15 -0500

    first commit

---
+++ test4/src/main/java/com/mycomp/mysample/test4/Employee.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycomp.mysample.test4;
+
+/**
+ *
+ * @author redychap
+ */
+public class Employee {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "name=" + name + '}';
+    }
+    
+    
+}
